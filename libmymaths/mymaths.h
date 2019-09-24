976eb18b974e08b1a7aa45e8ee2e82b1769bbc60
Thomas Arvanitidis <10086550+thomasarvanitidis@users.noreply.github.com>
2019-09-24 10:18:15 +0100

    Working Linux/macOS example

---
+++ libmymaths/mymaths.h
@@ -0,0 +1,17 @@
+// Author: Thomas Arvanitidis
+// URL:    https://github.com/tarvanitidis/rpath_tutorial
+
+#ifndef _MYMATHS_H_
+#define _MYMATHS_H_
+
+#include <vector>
+
+namespace mymaths
+{
+
+void yo();
+std::vector<double> add(std::vector<double>& a, std::vector<double>& b);
+
+} // End of namespace mymaths
+
+#endif // End of _MYMATHS_H_
